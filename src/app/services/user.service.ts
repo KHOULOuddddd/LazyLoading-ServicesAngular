48fd93909ba64b9fb04d13e52e414f79fa9f6737
Abir Talbi <abir.talbi@aosora.fr>
2022-01-20 17:50:23 +0100

    services

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
+//@Injectable(): Décorateur qui marque une classe comme disponible pour être fournie et injectée en tant que dépendance.
+
 @Injectable({
+
+//providedIn: Les injecteurs établissent la communication avec l'injectable qui est basé sur @NgModule ou un autre InjectorType, même en spécifiant que cet injectable doit être fourni dans l'injecteur 'root'
   providedIn: 'root'
 })
 export class UserService {
 
   constructor() { }
+  
+  getUsers() {
+    return [
+    {name : 'khouloud' , 'age': 25},
+    {name : 'khouloud' , 'age': 25},
+    {name : 'khouloud' , 'age': 25}
+  ]}
 }
