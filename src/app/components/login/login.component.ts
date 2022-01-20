995c2acdb1de055717b811216b0251f71af002b1
Abir Talbi <abir.talbi@aosora.fr>
2022-01-20 17:51:27 +0100

    services

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: []
   
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  table  : [] = [];
+  constructor(private userServices : UserService ) { }
+
 
   ngOnInit(): void {
+    console.log(this.userServices.getUsers);
   }
+
 
 }
